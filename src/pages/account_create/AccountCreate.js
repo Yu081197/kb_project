6e9045a0e2f50d0710a4c4d37cf908d5dbce3a5a
Yumalup <sonaki970811@gmail.com>
2022-10-05 17:39:02 +0900

    Add : AccountCreate 화면 구현

--- src/pages/account_create/AccountCreate.js
+++ src/pages/account_create/AccountCreate.js
@@ -1,7 +1,29 @@
 import React from "react";
+import "./AccountCreate.scss";
 
 function AccountCreate() {
-  return <div className="container">hi</div>;
+  return (
+    <div className="createContainer">
+      <div className="circleContainer">
+        <div className="circle">약관동의</div>
+        <div className="circle">정보입력</div>
+        <div className="circle">거래목적</div>
+        <div className="circle">비밀번호</div>
+        <div className="circle">본인인증</div>
+        <div className="circle">개설완료</div>
+      </div>
+
+      <div className="inputContainer">
+        <div>약관동의</div>
+        <div>전체동의하기</div>
+        <div>약관동의</div>
+        <div>약관동의</div>
+        <div>약관동의</div>
+        <div>약관동의</div>
+      </div>
+      <div className="circleContainer"></div>
+    </div>
+  );
 }
 
 export default AccountCreate;
