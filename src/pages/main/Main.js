85cc6a53c82d8c98617c87dc4b281a29048806f8
Yumalup <sonaki970811@gmail.com>
2022-10-05 09:20:23 +0900

    Add : main update

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,24 +1,18 @@
 import { Image } from "react-bootstrap";
-import { Slide } from "react-slideshow-image";
 import "react-slideshow-image/dist/styles.css";
 import "./Main.scss";
-
-const slideImages = [
-  {
-    url: "image/main_banner_1.png",
-    caption: "Slide 1",
-  },
-  {
-    url: "images/main_banner_2.jpg",
-    caption: "Slide 2",
-  },
-];
 
 function Main() {
   return (
     <div className="container">
       <div className="mainContainer">
-        <Image className="mainBox" src="image/main_banner_1.png" />
+        <div className="mainBox">
+          <div className="mainContainerSwiper">
+            <div className="innerSwiper">
+              <div className="item"></div>
+            </div>
+          </div>
+        </div>
 
         <div className="news">
           <div>news</div>
