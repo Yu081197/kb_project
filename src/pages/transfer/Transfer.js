009b6e436926230dda1a4b15af3e2783b5f0dfaa
Yumalup <sonaki970811@gmail.com>
2022-10-07 15:27:12 +0900

    Add : transfer 화면 구현완료

--- src/pages/transfer/Transfer.js
+++ src/pages/transfer/Transfer.js
@@ -2,35 +2,85 @@ import React from "react";
 import "./Transfer.scss";
 
 function Transfer() {
-  function handleClick(e) {
-    window.location.href = "/transfer";
-  }
   return (
-    <div className="lookupContainer">
-      <div className="lookupBox">
-        <div className="informBox">
-          <div className="informHeadBox">저축예금</div>
-          <div className="informSelectBox">
+    <div className="transferContainer">
+      <div className="transferBox">
+        <div className="transferInformBox">
+          <div className="transferInforHeadBox">
             <select>
-              <option>국민 : XXX - XXXXXX - XXX</option>
-              <option>국민 : XXX - XXXXXX - XXX</option>
-              <option>국민 : XXX - XXXXXX - XXX</option>
               <option>국민 : XXX - XXXXXX - XXX</option>
             </select>
           </div>
-          <div className="informMoneyBox">XX,XXX,XXX 원</div>
-          <div className="informPossibleBox">출금가능잔액 : XX,XXX,XXX원</div>
-          <div className="informButton">
-            <button onClick={handleClick}>이체하기</button>
+          <div className="transferInformMoneyBox">금액 : XX,XXX,XXX 원</div>
+          <div className="transferInformPossibleBox">
+            출금가능잔액 : XX,XXX,XXX원
           </div>
         </div>
 
-        <div className="listBox">
-          <div className="listHead">
-            <div>월별내역 : </div>
-            <div className="listHeadCalender">Calender</div>
+        <div className="transfer">
+          <div className="transferRecentBox">
+            <div className="transferRecentHead">
+              <div>최근이체내역</div>
+            </div>
+
+            <div className="transferRecentContainer">
+              <div className="transferRecentList">
+                <div className="transferRecentListDate">
+                  20XX - XX - XX (목)
+                </div>
+                <div className="transferRecentListTime">12 : 30 : 49</div>
+                <div className="transferRecentListBox">
+                  <div className="transferRecentListName">도깨비</div>
+                  <div className="transferRecentListAmount">XX,XXX 원</div>
+                </div>
+                <div className="transferRecentListBalance">
+                  잔액 : XX,XXX,XXX 원
+                </div>
+              </div>
+
+              <div className="transferRecentList">
+                <div className="transferRecentListDate">
+                  20XX - XX - XX (목)
+                </div>
+                <div className="transferRecentListTime">12 : 30 : 49</div>
+                <div className="transferRecentListBox">
+                  <div className="transferRecentListName">도깨비</div>
+                  <div className="transferRecentListAmount">XX,XXX 원</div>
+                </div>
+                <div className="transferRecentListBalance">
+                  잔액 : XX,XXX,XXX 원
+                </div>
+              </div>
+
+              <div className="transferRecentList">
+                <div className="transferRecentListDate">
+                  20XX - XX - XX (목)
+                </div>
+                <div className="transferRecentListTime">12 : 30 : 49</div>
+                <div className="transferRecentListBox">
+                  <div className="transferRecentListName">도깨비</div>
+                  <div className="transferRecentListAmount">XX,XXX 원</div>
+                </div>
+                <div className="transferRecentListBalance">
+                  잔액 : XX,XXX,XXX 원
+                </div>
+              </div>
+            </div>
           </div>
-          <div>list</div>
+
+          <div className="opponentAcoount">
+            {/* input select 동시에 써야함*/}
+            <select>
+              <option>농협 : XXX - XXXXXX - XXX</option>
+            </select>
+          </div>
+
+          <div className="transferAmountBox">
+            <div className="transferAmount">XX,XXX,XXX 원</div>
+            <div>
+              <button>이체</button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
