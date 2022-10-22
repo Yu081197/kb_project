cf9ef4ab88c3010594961f7d16342aad756b4b59
waterricecake <91263263+waterricecake@users.noreply.github.com>
2022-10-22 14:10:53 +0900

    Add : login logout 구현

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,60 +1,130 @@
+import { useState } from "react";
 import { Image } from "react-bootstrap";
 import "./Nav.scss";
 import { Link, NavLink } from "react-router-dom";
+import axios from "axios";
+const getCookieValue = (key) => {
+  let cookieKey = key + "=";
+  let result = "";
+  const cookieArr = document.cookie.split(";");
 
+  for (let i = 0; i < cookieArr.length; i++) {
+    if (cookieArr[i][0] === " ") {
+      cookieArr[i] = cookieArr[i].substring(1);
+    }
+
+    if (cookieArr[i].indexOf(cookieKey) === 0) {
+      result = cookieArr[i].slice(cookieKey.length, cookieArr[i].length);
+      return result;
+    }
+  }
+  return result;
+};
+function logout() {
+  axios
+    .post(
+      "/logout",
+      {},
+      {
+      },
+      {
+        headers: {
+          "content-type": "application/json",
+        },
+      }
+    )
+    .then(function (response) {
+      console.log("성공");
+      window.location.reload();
+    })
+    .catch(function (error) {
+      console.log("실패");
+    });
+    
+};
+const login_click = () =>{
+  if (getCookieValue("name").length > 0) {
+      logout();
+    } 
+    else {
+      window.location.href = "/login";
+    };
+}
 function Nav() {
+
+  const login = () => {
+    let login_name = "로그인을 해주세요";
+    let user_name = getCookieValue("name");
+    let login_text = "Login"
+    if (user_name.length > 0) {
+      login_name = "안녕하세요 " + user_name + "님";
+      login_text = "Logout"
+    }
+    let login_data = [login_name,login_text]
+    return login_data;
+  }
+  var login_data = login();
+  var login_name =login_data[0]
+  var login_text = login_data[1]
+
+
   return (
     <div>
       <div className="header">
         <Link to="/">
           <Image className="mainLogo" src="image/kbmain.png" />
         </Link>
-        <div className="headerLogin">
-          <div>안녕하세요 도깨비님</div>
+        <div className="navHeader">
+          <div className="nav">
+            <NavLink
+              exact
+              to="/account_create"
+              style={{ textDecoration: "none", color: "black" }}
+              className="navTab"
+            >
+              계좌개설
+            </NavLink>
+            <NavLink
+              to="/account_lookup"
+              style={{ textDecoration: "none", color: "black" }}
+              activeClassName="active"
+              className="navTab"
+            >
+              계좌조회
+            </NavLink>
+            <NavLink
+              to="/transfer"
+              style={{ textDecoration: "none", color: "black" }}
+              className="navTab"
+              activeClassName="active"
+            >
+              이체
+            </NavLink>
+            <NavLink
+              to="/find"
+              style={{ textDecoration: "none", color: "black" }}
+              className="navTab"
+              activeClassName="active"
+            >
+              점포찾기
+            </NavLink>
+            <NavLink
+              to="/predict"
+              style={{ textDecoration: "none", color: "black" }}
+              className="navTab"
+              activeClassName="active"
+            >
+              신용등급예측
+            </NavLink>
+          </div>
+          <div className="headerLogin">
+            <div>{login_name}</div>
+          </div>
+          <div onClick= {login_click}>
+            {login_text}
+          </div>
         </div>
-      </div>
 
-      <div className="nav">
-        <NavLink
-          exact
-          to="/account_create"
-          style={{ textDecoration: "none", color: "black" }}
-          className="navTab"
-        >
-          계좌개설
-        </NavLink>
-        <NavLink
-          to="/account_lookup"
-          style={{ textDecoration: "none", color: "black" }}
-          activeClassName="active"
-          className="navTab"
-        >
-          계좌조회
-        </NavLink>
-        <NavLink
-          to="/transfer"
-          style={{ textDecoration: "none", color: "black" }}
-          className="navTab"
-          activeClassName="active"
-        >
-          이체
-        </NavLink>
-        <NavLink
-          to="/find"
-          style={{ textDecoration: "none", color: "black" }}
-          className="navTab"
-          activeClassName="active"
-        >
-          점포찾기
-        </NavLink>
-        <NavLink
-          to="/predict"
-          style={{ textDecoration: "none", color: "black" }}
-          className="navTab"
-          activeClassName="active"
-        >
-          신용등급예측
-        </NavLink>
       </div>
     </div>
   );
---
+++ src/pages/login/Login.js
@@ -0,0 +1,75 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./Login.scss";
+
+function Login() {
+  const [accountNumberState, setAccountNumberState] = useState();
+  const [passwordState, setPasswordState] = useState();
+
+  const onChangeAccountNumber = (e) => {
+    setAccountNumberState(e.target.value);
+  };
+
+  const onChangePassword = (e) => {
+    setPasswordState(e.target.value);
+  };
+
+  function validLogin() {
+    axios
+      .post(
+        "/login",
+        {},
+        {
+          params: {
+            accountNumber: accountNumberState,
+            password: passwordState,
+          },
+        },
+        {
+          headers: {
+            "content-type": "application/json",
+          },
+        }
+      )
+      .then(function (response) {
+        console.log(response.data.account_number);
+        console.log("성공");
+        window.location.href = "/main";
+      })
+      .catch(function (error) {
+        console.log("실패");
+      });
+  }
+  return (
+    <div className="loginContainer">
+      <div className="loginHeader">로 그 인</div>
+      <div class="all_cover">
+        <div class="login-form">
+          <form>
+            <input
+              type="text"
+              name="accountNumber"
+              class="text-field"
+              placeholder="계좌번호"
+              value={accountNumberState}
+              onChange={onChangeAccountNumber}
+            />
+            <input
+              type="password"
+              name="password"
+              class="text-field"
+              placeholder="비밀번호"
+              value={passwordState}
+              onChange={onChangePassword}
+            />
+            <button type="button" class="submit-btn" onClick={validLogin}>
+              로그인
+            </button>
+          </form>
+        </div>
+      </div>{" "}
+    </div>
+  );
+}
+
+export default Login;
