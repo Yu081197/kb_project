2823ccc92e621a609715c93aada882079dcae646
Yu081197 <81637903+Yu081197@users.noreply.github.com>
2022-10-24 09:32:11 +0900

    Add : chatbot
    ACInformChatbot 추가

---
+++ src/pages/account_create/accountCreateChatbot/ACInformChatbot.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./ACInformChatbot.scss";
+import $ from "jquery";
+
+class ACInformChatbot extends React.Component {
+  componentDidMount() {}
+  render() {
+    return;
+  }
+}
+
+export default ACInformChatbot;
