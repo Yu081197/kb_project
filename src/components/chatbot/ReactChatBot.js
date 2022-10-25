b85c5c8d2b3f8452d19fba095b4c20fe420fe81f
Yu081197 <81637903+Yu081197@users.noreply.github.com>
2022-10-25 10:34:52 +0900

    Update ReactChatBot.js

--- src/components/chatbot/ReactChatBot.js
+++ src/components/chatbot/ReactChatBot.js
@@ -3,18 +3,21 @@ import { Image } from "react-bootstrap";
 import "./ChatBot.scss";
 
 function ReactChatBot() {
-    useEffect(() => {
-        console.log("useEffect :: ReactChatBot");
-        let useVoiceService = localStorage.getItem("useVoiceService");
-        // console.log("useVoiceService: " + useVoiceService);
-        if (useVoiceService == null) {  // 선택한 서비스가 없는 경우 서비스선택요청
-            speak(
-                "어떤 서비스를 이용하시겠습니까? 일번 음성서비스, 이번 헤드트래킹, 삼번 이용안함 중 선택해주세요.", true, recogInputService
-            );
-        } else if (useVoiceService == "true") {
-            recognitionStart();
-        }
-    }, []);
+  useEffect(() => {
+    console.log("useEffect :: ReactChatBot");
+    let useVoiceService = localStorage.getItem("useVoiceService");
+    // console.log("useVoiceService: " + useVoiceService);
+    if (useVoiceService == null) {
+      // 선택한 서비스가 없는 경우 서비스선택요청
+      speak(
+        "어떤 서비스를 이용하시겠습니까? 일번 음성서비스, 이번 헤드트래킹, 삼번 이용안함 중 선택해주세요.",
+        true,
+        recogInputService
+      );
+    } else if (useVoiceService == "true") {
+      recognitionStart();
+    }
+  }, []);
 
   return (
     <div>
@@ -123,90 +126,50 @@ recognition.onend = function (event) {
 
   let result_remove_blank = result.replaceAll(" ", "");
 
+  // 강제종료
+  if (result.includes("꺼져")) {
+    console.log("===== 음성인식 강제종료 =====");
+    localStorage.setItem("useVoiceService", false);
+    recognition.stop();
+    return;
+  }
 
-    // 강제종료
-    if (result.includes("꺼져")) {
-        console.log("===== 음성인식 강제종료 =====");
+  // 깨비 호출
+  if (result.includes("깨비")) {
+    console.log("===== 꺠비호출 =====");
+    speak("네, 말씀하세요");
+  }
+
+  // 입력인 경우
+  else if (inputOption != null) {
+    /* 서비스 선택 */
+    if (inputOption == recogInputService) {
+      console.log("===== 서비스 선택 =====");
+      if (result_remove_blank.includes("음성")) {
+        // 음성서비스 사용설정
+        localStorage.setItem("useVoiceService", true);
+        inputOption = null;
+      } else if (
+        result_remove_blank.includes("헤드트래킹") ||
+        result_remove_blank.includes("헤드트레킹")
+      ) {
+        // 음성서비스 미사용설정 및 헤드트레킹 다운로드
         localStorage.setItem("useVoiceService", false);
+        inputOption = null;
+        recognition.stop();
+        // 헤드트레킹 다운로드 기능 추가
+        return;
+      } else if (
+        result_remove_blank.includes("이용안함") ||
+        result_remove_blank.includes("사용안함")
+      ) {
+        // 음성서비스 미사용설정
+        localStorage.setItem("useVoiceService", false);
+        inputOption = null;
         recognition.stop();
         return;
-    }
-
-    // 깨비 호출
-    if (result.includes("깨비")) {
-        console.log("===== 꺠비호출 =====");
-        speak("네, 말씀하세요");
-    }
-
-    // 입력인 경우
-    else if (inputOption != null) {
-        /* 서비스 선택 */
-        if (inputOption == recogInputService) {
-            console.log("===== 서비스 선택 =====");
-            if (result_remove_blank.includes("음성")) { // 음성서비스 사용설정
-                localStorage.setItem("useVoiceService", true);
-                inputOption = null;
-            } else if (result_remove_blank.includes("헤드트래킹") || result_remove_blank.includes("헤드트레킹")) {  // 음성서비스 미사용설정 및 헤드트레킹 다운로드
-                localStorage.setItem("useVoiceService", false);
-                inputOption = null;
-                recognition.stop();
-                // 헤드트레킹 다운로드 기능 추가
-                return;
-            } else if (result_remove_blank.includes("이용안함") || result_remove_blank.includes("사용안함")) {  // 음성서비스 미사용설정
-                localStorage.setItem("useVoiceService", false);
-                inputOption = null;
-                recognition.stop();
-                return;
-            } else {
-                speak("이해하지 못했습니다.");
-                recognition.start();
-            }
-        }
-
-        /* 약관동의 */
-        else if (inputOption == recogInputAgree) {   // 2
-            console.log("===== 약관동의 =====");
-            if (result_remove_blank.includes("예") || result_remove_blank.includes("네")) {
-                document.querySelector("#allAgree").checked = true;
-                document.querySelector("#useAgree").checked = true;
-                document.querySelector("#itemAgree").checked = true;
-                document.querySelector("#specialAgree").checked = true;
-                document.querySelector("#basicAgree").checked = true;
-                document.querySelector("#freeAgree").checked = true;
-                document.querySelector("#personalAgree").checked = true;
-                inputOption = null;
-            }
-        }
-
-        /* 계좌개설 :: 다음페이지로 이동 */
-        else if (inputOption == recogInputNext) {   // 3
-            console.log("===== 계좌개설 :: 다음페이지로 이동 =====");
-            if (result_remove_blank.includes("예") || result_remove_blank.includes("네")) {
-                window.location.href = inputArg;
-                inputOption = null;
-            }
-        }
-    }
-
-    // 페이지 이동
-    else if (result.includes("이동")) {
-        console.log("===== 페이지이동 =====");
-
-        for (let key of pages.keys()) {
-            if (result_remove_blank.includes(key)) {
-                window.location.href = pages.get(key);
-            }
-        }
-    }
-
-    // 데이터 입력
-
-    // 속도조절
-    
-
-    var useVoiceService = localStorage.getItem("useVoiceService");
-    if (useVoiceService == "true") {
-
+      } else {
+        speak("이해하지 못했습니다.");
         recognition.start();
       }
     } else if (inputOption == recogInputAgree) {
@@ -257,8 +220,8 @@ recognition.onend = function (event) {
 
   // 속도조절
 
-  console.log("recong_ing: " + recong_ing);
-  if (recong_ing) {
+  var useVoiceService = localStorage.getItem("useVoiceService");
+  if (useVoiceService == "true") {
     recognition.start();
   }
 };
@@ -272,7 +235,6 @@ recognition.onerror = function (event) {
 recognition.onnomatch = function (event) {
   console.log("SpeechRecognition.onnomatch");
 };
-
 
 // ================== TTS settings ==================
 let pitch = 1; // 음높이
@@ -303,10 +265,8 @@ if (speechSynthesis.onvoiceschanged !== undefined) {
 }
 
 export function recognitionStart() {
-
-    console.log("recognition.start() in recognitionStart");
-    recognition.start();
-
+  console.log("recognition.start() in recognitionStart");
+  recognition.start();
 }
 
 export function speak(
@@ -342,6 +302,7 @@ export function speak(
 
   utterThis.onend = function (event) {
     console.log("SpeechSynthesisUtterance.onend");
+    console.log("SpeechSynthesisUtterance.onend :: recognition.start");
     recognition.start();
   };
 
@@ -359,41 +320,10 @@ export function speak(
     }
   }
 
-    if (isInput) {
-        inputOption = recogInputOption;
-        inputArg = arg;
-    }
-
-    const utterThis = new SpeechSynthesisUtterance(inputTxt);
-
-    utterThis.onstart = function (event) {
-        console.log("SpeechSynthesisUtterance.onstart");
-        console.log("SpeechSynthesisUtterance.onstart :: recognition.stop");
-        recognition.stop();
-    };
-
-    utterThis.onend = function (event) {
-        console.log("SpeechSynthesisUtterance.onend");
-        console.log("SpeechSynthesisUtterance.onend :: recognition.start");
-        recognition.start();
-    };
-
-    utterThis.onerror = function (event) {
-        console.error("SpeechSynthesisUtterance.onerror");
-        console.log(`An error has occurred with the speech synthesis: ${event.error}`);
-    };
-
-    for (let i = 0; i < voices.length; i++) {
-        if (voices[i].name === voiceNameKO) {
-            utterThis.voice = voices[i];
-            break;
-        }
-    }
-
-    utterThis.pitch = pitch;
-    utterThis.rate = rate;
-    synth.speak(utterThis);
-    console.log("speak: ", inputTxt);
+  utterThis.pitch = pitch;
+  utterThis.rate = rate;
+  synth.speak(utterThis);
+  console.log("speak: ", inputTxt);
 }
 
 export default ReactChatBot;
