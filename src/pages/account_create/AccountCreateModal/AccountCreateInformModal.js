30cc1285045af4605c17e2739798ef5d20647620
Yu081197 <81637903+Yu081197@users.noreply.github.com>
2022-10-27 00:35:34 +0900

    Update AccountCreateInformModal.js

--- src/pages/account_create/AccountCreateModal/AccountCreateInformModal.js
+++ src/pages/account_create/AccountCreateModal/AccountCreateInformModal.js
@@ -10,7 +10,8 @@ const AccountCreateInformModal = ({ setModalOpen }) => {
     <div className="accountCreateModalContainer">
       <div className="accountCreateModalBox">
         <div className="accountCreateModal">
-          모든 항목을 올바르게 입력해주세요
+          모든 항목을 <br />
+          올바르게 입력해주세요
         </div>
         <div className="accountCreateModalButton">
           <button onClick={closeModal}>확인</button>
